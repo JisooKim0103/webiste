e2dbc79b194245f36f05dabb8575ab1dc5ed4d11
shin yeon lee <89951814+JisooKim0103@users.noreply.github.com>
2022-04-12 19:43:44 +0800

    website thesis

---
+++ homepage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Southeastern College Student Account Management</title>
+    <link rel="stylesheet" href="style.css">    
+</head>
+<body>
+    <header>
+    <div class="wrapper">
+        <div class="logo">
+            <img src="P:\website\Homepage\logo.jpg" alt="">
+        </div>
+<ul class="nav-area">
+<li><a href="#">Dashboard</a></li>
+<li><a href="#"></a>Student Guide</li>
+<li><a href="#"></a>Bulletin Board </li>
+<li><a href="#">Profile</a></li>
+<li><a href="#">Settings</a></li>
+</ul>
+</div>
+<div class="welcome-text">
+        <h1>
+We are <span>WELCOME TO SOUTHEASTERN COLLEGE</span></h1>
+<a href="#">Contact US</a>
+    </div>
+</header>
+
+</body>
+</html>
